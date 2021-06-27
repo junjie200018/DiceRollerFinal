9fcb56255acae0a20dc49662cf39e364b97c68e7
wongjunjie18 <wongjj-am18@student.tarc.edu.my>
2021-06-27 17:15:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dicerollerfinal/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.dicerollerfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
+
+        val btnRoll : Button = findViewById(R.id.roll_button)
+
+        btnRoll.setOnClickListener(){
+            roolDice()
+        }
+    }
+
+    private fun roolDice(){
+        val randomInt = (1..6).random()
+
+
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+    }
+
+
+}
